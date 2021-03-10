a2467e1f185769a420b5453953410bb27864f679
Jarek Goszowski <goszowski@gmail.com>
2021-03-10 19:55:03 +0200

    first commit

---
+++ app/Http/Controllers/ProxyController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Ixudra\Curl\Facades\Curl;
+use Illuminate\Support\Arr;
+
+class ProxyController extends Controller
+{
+    public function handle(Request $request)
+    {
+        $query = Arr::query($request->query());
+
+        $headers = [];
+
+        foreach($request->header() as $key=>$arr)
+        {
+            if(isset($arr[0]))
+            {
+                $headers = array_merge($headers, [$key => $arr[0]]);
+            }
+        }
+
+        $response = Curl::to(config('proxy.url') . $request->path() . ($query ? '?'.$query : null))
+            ->withData($request->getContent())
+            ->withHeaders($headers)
+            ->allowRedirect()
+            ->returnResponseObject()
+            ->{$request->method()}();
+
+        return response($response->content, $response->status)->header('Content-Type', $response->contentType);
+    }
+}
