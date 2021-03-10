2b2cb100574a7704b11ad71a89cc418d8b889768
Jarek Goszowski <goszowski@gmail.com>
2021-03-10 21:30:01 +0200

    Видалви заголовки взагалі

--- app/Http/Controllers/ProxyController.php
+++ app/Http/Controllers/ProxyController.php
@@ -12,24 +12,9 @@ class ProxyController extends Controller
     {
         $query = Arr::query($request->query());
 
-        $headers = [];
-
-        foreach($request->header() as $key=>$arr)
-        {
-            if(isset($arr[0]))
-            {
-                $headers = array_merge($headers, [$key => $arr[0]]);
-            }
-        }
-
-        if(isset($headers['host']))
-            unset($headers['host']);
-
         $response = Curl::to(config('proxy.url') . $request->path() . ($query ? '?'.$query : null))
             ->withData($request->getContent())
             ->withResponseHeaders()
-            ->withHeaders($headers)
-            // ->allowRedirect()
             ->returnResponseObject()
             ->{$request->method()}();
 
